6fe788c536f417df96246600b9bac82bba0b7663
spence521 <spence521@yahoo.com>
2017-09-12 16:11:56 -0600

    Add files via upload

--- Assignment_1_submission/Assignment_1.sh
+++ Assignment_1_submission/Assignment_1.sh
@@ -21,18 +21,25 @@ mv Part_2/Data.exe .
 echo "2 (A)"
 printf "Depth set to 1: "
 mono Data.exe updated_training00.txt updated_training01.txt updated_training02.txt updated_training03.txt 1
+echo
 printf "Depth set to 2: "
 mono Data.exe updated_training00.txt updated_training01.txt updated_training02.txt updated_training03.txt 2
+echo
 printf "Depth set to 3: "
 mono Data.exe updated_training00.txt updated_training01.txt updated_training02.txt updated_training03.txt 3
+echo
 printf "Depth set to 4: "
 mono Data.exe updated_training00.txt updated_training01.txt updated_training02.txt updated_training03.txt 4
+echo
 printf "Depth set to 5: "
 mono Data.exe updated_training00.txt updated_training01.txt updated_training02.txt updated_training03.txt 5
+echo
 printf "Depth set to 10: "
 mono Data.exe updated_training00.txt updated_training01.txt updated_training02.txt updated_training03.txt 10
+echo
 printf "Depth set to 15: "
 mono Data.exe updated_training00.txt updated_training01.txt updated_training02.txt updated_training03.txt 15
+echo
 printf "Depth set to 20: "
 mono Data.exe updated_training00.txt updated_training01.txt updated_training02.txt updated_training03.txt 20
 
